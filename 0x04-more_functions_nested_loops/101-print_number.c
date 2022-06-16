e82e1891cf104ffd2619820e484b015779d8fd82
Nala-m <marylindao@yahoo.com>
2022-06-16 09:34:49 -0700

    updating branches

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,9 +15,8 @@ void print_number(int n)
 	if (n == 0)
 		putchar('0');
 
-	if (n >= 10)
-		putchar(n);
-
 	if (n / 10)
+		printnumber(n / 10);
+
 		putchar(n % 10 + '0');
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-This is a project that teaches about debugging.
-The project checks for bugs in the previous task If_else variables.

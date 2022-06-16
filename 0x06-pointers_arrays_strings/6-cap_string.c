98d70ce5e316a4e166a6fb620e71e6aed71cd5b0
Nala-m <marylindao@yahoo.com>
2022-06-16 09:22:35 -0700

    answer 6 cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * cap_string - capitalizes all words of a string
- * @s: input string.
+ * @x: input string.
  * Return: the pointer to dest.
  */
 
@@ -11,14 +11,14 @@ char *cap_string(char *x)
 	char spc = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
 	int len = 13;
 	int a = 0, i;
-	
+
 	while (x[a])
 	{
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || s[a-1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] = s[a] - 32;
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				x[a] = x[a] - 32;
 			i++;
 		}
 		a++;

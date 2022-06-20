cb2e1df045e2ada91103e65e4def13cb4c70befa
Nala-m <marylindao@yahoo.com>
2022-06-20 07:34:31 -0700

    answer 2 strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,6 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	p = strchr(s, c);
-	return (s);
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
 }

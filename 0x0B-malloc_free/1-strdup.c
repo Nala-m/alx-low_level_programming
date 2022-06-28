97090edf8ea802c87f8f6636b8d055ecc0146dfe
Nala-m <marylindao@yahoo.com>
2022-06-28 04:30:06 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string and sets a pointer to duplicate
+ *
+ * @str: string to copy
+ *
+ * Return: a pointer to duplicate string or else NULL
+ *
+ */
+char *_strdup(char *str)
+{
+	char *result;
+
+	result = _strdup(str);
+	return (result);
+}
+
